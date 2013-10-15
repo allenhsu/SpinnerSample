81741be2b5bdf115c07db0ac80a789eb7962dc8a
Allen Hsu <me@imallen.com>
2013-10-15 16:33:56 +0800

    Initial Commit

---
+++ SpinnerSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpinnerSample
+//
+//  Created by Allen Hsu on 10/15/13.
+//  Copyright (c) 2013 Allen Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
